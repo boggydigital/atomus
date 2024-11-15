6da595fc75879c92feee2ae7d4a725bc90ad3e5b
Bogdan Brinza <Bogdan.Brinza@hey.com>
2024-11-14 16:20:52 -0800

    Making feed and entry link optional

--- feed.go
+++ feed.go
@@ -16,11 +16,10 @@ const (
 type Feed struct {
 	XMLName      xml.Name `xml:"feed"` // <feed xmlns="http://www.w3.org/2005/Atom">
 	XMLNamespace string   `xml:"xmlns,attr"`
-	Title        string   `xml:"title"` // <title>Example Feed</title>
-	//Subtitle     string   `xml:"subtitle,omitempty"`
-	Link    *Link  `xml:"link,omitempty"` //<link href="http://example.org/" />
-	Updated string `xml:"updated"`        // <updated>2003-12-13T18:30:02Z</updated>
-	Entry   *Entry `xml:"entry"`
+	Title        string   `xml:"title"`          // <title>Example Feed</title>
+	Link         *Link    `xml:"link,omitempty"` //<link href="http://example.org/" />
+	Updated      string   `xml:"updated"`        // <updated>2003-12-13T18:30:02Z</updated>
+	Entry        *Entry   `xml:"entry"`
 }
 
 type Link struct {
@@ -28,26 +27,26 @@ type Link struct {
 	Href    string   `xml:"href,attr"`
 }
 
-func NewFeed(title, link string) *Feed {
-	return &Feed{
+func NewFeed(title, href string) *Feed {
+	feed := &Feed{
 		XMLNamespace: atomXMLNS,
 		Title:        title,
-		Link: &Link{
-			Href: link,
-		},
 	}
+
+	if href != "" {
+		feed.Link = &Link{Href: href}
+	}
+
+	return feed
 }
 
-func (f *Feed) SetEntry(title, author, link, content string) {
+func (f *Feed) SetEntry(title, author, href, content string) {
 	updated := time.Now()
 	updatedRFC3339 := updated.Format(time.RFC3339)
 
 	f.Updated = updatedRFC3339
 	f.Entry = &Entry{
-		Title: fmt.Sprintf("%s - %s", title, updated.Format(time.RFC1123)),
-		Link: &Link{
-			Href: link,
-		},
+		Title:     fmt.Sprintf("%s - %s", title, updated.Format(time.RFC1123)),
 		Id:        strconv.FormatInt(updated.Unix(), 10),
 		Published: updatedRFC3339,
 		Updated:   updatedRFC3339,
@@ -58,6 +57,10 @@ func (f *Feed) SetEntry(title, author, link, content string) {
 			Type:    atomContentType,
 			Content: content,
 		},
+	}
+
+	if href != "" {
+		f.Entry.Link = &Link{Href: href}
 	}
 }
 
